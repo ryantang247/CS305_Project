15d14e4d3272526002933dcc0fe39fc7dcb42faa
nopyesok <emailbryan2000@gmail.com>
2023-12-27 18:19:21 +0800

    add encryption

--- httpserver.py
+++ httpserver.py
@@ -1,3 +1,5 @@
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import socket
 import base64
 import os
@@ -5,12 +7,38 @@ from urllib.parse import urlparse, parse_qs, unquote
 import hashlib
 from datetime import datetime
 import threading
-
+import json
+import rsa
+import time
 account = {'client1': '123', 'client2': '123', 'client3': '123'}
-
+publicKey, privateKey = rsa.newkeys(512)
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 print(f"The directory of the current file is: {current_directory}")
+
+session_keys = {}
+
+# Function to handle new client connections
+
+
+def clear_session_keys():
+    global session_keys
+    while True:
+        time.sleep(300)  # Sleep for 5 minutes (300 seconds)
+        session_keys = {}  # Clear the dictionary
+
+# Start a separate thread to clear the session_keys
+clear_keys_thread = threading.Thread(target=clear_session_keys)
+clear_keys_thread.daemon = True  # Set the thread as a daemon so it exits when the main program ends
+clear_keys_thread.start()
+
+# Function to handle client disconnection
+
+
+def handle_client_disconnection(session_id):
+    # Remove the key associated with the session ID upon disconnection
+    if session_id in session_keys:
+        del session_keys[session_id]
 
 
 def process_url(url):
@@ -87,7 +115,8 @@ def get_file_list(directory_path):
         entries = os.listdir(directory_path)
 
         # Filter out directories, leaving only files
-        files = [entry for entry in entries if os.path.isfile(os.path.join(directory_path, entry))]
+        files = [entry for entry in entries if os.path.isfile(
+            os.path.join(directory_path, entry))]
 
         return files
     except OSError as e:
@@ -102,7 +131,8 @@ session_storage = {}
 def handle_login(username, password):
     # Validate username and password (your authentication logic)
     # If login successful:
-    session_id = generate_unique_session_id(username)  # Generate a unique session ID
+    session_id = generate_unique_session_id(
+        username)  # Generate a unique session ID
     session_storage[username] = session_id  # Store session ID for the user
     return session_id
 
@@ -177,7 +207,8 @@ def extractData(request_data):
     content_length = int(headers.get("Content-Length", 0))
 
     # Extract data from the request body
-    data_start = request_data.find("\r\n\r\n") + 4  # Find the position where data starts
+    # Find the position where data starts
+    data_start = request_data.find("\r\n\r\n") + 4
     data = request_data[data_start:data_start + content_length]
 
     return data
@@ -201,6 +232,29 @@ def isDeleteMethod(url):
         return False
 
 
+def symmetric_decrypt(encrypted_data, key, iv):
+    # Convert the key and IV into bytes if they're not already in bytes format
+    key = key.encode() if isinstance(key, str) else key
+    iv = iv.encode() if isinstance(iv, str) else iv
+
+    # Ensure the key is 16, 24, or 32 bytes long (AES-128, AES-192, or AES-256)
+    if len(key) not in [16, 24, 32]:
+        raise ValueError(
+            "Key must be 16, 24, or 32 bytes long for AES decryption")
+
+    # Create an AES cipher object for decryption
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv),
+                    backend=default_backend())
+
+    # Create a decryptor
+    decryptor = cipher.decryptor()
+
+    # Decrypt the message
+    decrypted_data = decryptor.update(encrypted_data) + decryptor.finalize()
+
+    return decrypted_data
+
+
 def handle_client_request(client_socket):
     # Receive data from the client
     # incr = 1;
@@ -221,7 +275,8 @@ def handle_client_request(client_socket):
     try:
         print('cookie', extractHeader(request_data)['Cookie'])
         if extractHeader(request_data)['Cookie']:
-            username = get_key_from_value(session_storage, extractHeader(request_data)['Cookie'])
+            username = get_key_from_value(
+                session_storage, extractHeader(request_data)['Cookie'])
             if username is None:
                 authenticated = authenticate(extractHeader(request_data))
                 if not authenticated:
@@ -260,7 +315,27 @@ def handle_client_request(client_socket):
 
     # Implement logic based on the HTTP method
     if method == "GET":
+        if url == "/public_key":
+            # Convert the server's public key to bytes
+            server_public_key_bytes = publicKey.save_pkcs1(
+                format='PEM')
 
+            # Send the server's public key as a response
+            headers = {
+                "Content-Length": str(len(server_public_key_bytes)),
+                "Content-Type": "application/octet-stream",
+                "Connection": "keep-alive",
+            }
+            response_status_line = "HTTP/1.1 200 OK\r\n"
+            response_header = ""
+            for header, value in headers.items():
+                response_header += f"{header}: {value}\r\n"
+            response_header += "\r\n"
+
+            # Send the response status line, headers, and public key
+            client_socket.sendall(
+                (response_status_line + response_header).encode('utf-8'))
+            client_socket.sendall(server_public_key_bytes)
         path = current_directory + url
         # if url == "/":
         # Using a raw string
@@ -283,7 +358,8 @@ def handle_client_request(client_socket):
         response_header += "\r\n"
 
         # Send the response status line, headers, and HTML content
-        client_socket.sendall((response_status_line + response_header).encode('utf-8'))
+        client_socket.sendall(
+            (response_status_line + response_header).encode('utf-8'))
         client_socket.sendall(html_content.encode('utf-8'))
         # elif url.startswith("/files"):
         #     handle_file_request(client_socket, url)
@@ -302,13 +378,14 @@ def handle_client_request(client_socket):
             # File exists, construct the headers
 
             headers = {
-                "Content-Length": str(os.path.getsize(path)),  # Assuming file size is the content length for HEAD
+                # Assuming file size is the content length for HEAD
+                "Content-Length": str(os.path.getsize(path)),
                 "Content-Type": "application/octet-stream",  # Modify as per your file type
                 "Connection": "keep-alive",
                 "Set-Cookie": f"session_id={session_id}; HttpOnly; Path=/",
             }
 
-            response_status_line = "HTTP/1.1 200 OK\r\n"
+            response_status_line = "HTTP/1.1 206 OK\r\n"
 
             response_header = ""
 
@@ -319,18 +396,75 @@ def handle_client_request(client_socket):
 
             # Send the response status line and headers (without body)
 
-            client_socket.sendall((response_status_line + response_header).encode('utf-8'))
+            client_socket.sendall(
+                (response_status_line + response_header).encode('utf-8'))
 
         else:
             # File doesn't exist, handle accordingly with a 404 Not Found response
             error_response = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found"
             client_socket.sendall(error_response.encode('utf-8'))
 
-
-
     elif method == "POST":
         # Handle POST request - receive data from the client
+        
         try:
+            if url == "/receive_key":
+            # Receive encrypted symmetric key from client
+                session_id = extractHeader(request_data)['Session-ID']
+                json_start = request_data.find('{')
+                json_data = request_data[json_start:]
+
+                # Extract the encrypted key from the JSON payload
+                encrypted_symmetric_key_base64 = json.loads(json_data).get(
+                    'encrypted_key', '')  # Convert bytes to string
+
+                # Get the encrypted symmetric key from the JSON payload
+
+                # Convert the Base64 string to bytes for decryption
+                encrypted_symmetric_key = base64.b64decode(
+                    encrypted_symmetric_key_base64)
+
+                # Decrypt the symmetric key using the server's private key
+                decrypted_symmetric_key = rsa.decrypt(
+                    encrypted_symmetric_key, privateKey)
+
+                # Now you have the decrypted symmetric key as bytes
+                # You can use it for further symmetric encryption/decryption
+                response_data = decrypted_symmetric_key
+                session_keys[session_id] = decrypted_symmetric_key
+    # Add necessary HTTP response headers
+                response_headers = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + \
+                    str(len(response_data)) + "\r\n\r\n"
+
+    # Combine headers and response data
+                response = response_headers.encode('utf-8') + response_data
+
+                # Send the response to the client
+                client_socket.sendall(response)
+                return
+            if url == "/testing":
+                json_start = request_data.find('{')
+                json_data = request_data[json_start:]
+
+                # Extract the encrypted key from the JSON payload
+
+                decrypted_symmetric_key_client = session_keys[extractHeader(request_data)[
+                    'Session-ID']]
+
+                encrypted_data_client = json.loads(json_data).get(
+                    'encrypted_data', '')
+                IV = extractHeader(request_data)[
+                    'IV']
+                decrypted_message = symmetric_decrypt(bytes.fromhex(encrypted_data_client), decrypted_symmetric_key_client, bytes.fromhex(IV))
+                response_headers = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + \
+                    str(len(decrypted_message)) + "\r\n\r\n"
+
+    # Combine headers and response data
+                response = response_headers.encode('utf-8') + decrypted_message
+
+                # Send the response to the client
+                client_socket.sendall(response)
+                return
             _headers = extractHeader(request_data)
             content_length = _headers.get("Content-Length")
             if content_length is None:
@@ -385,7 +519,8 @@ def handle_client_request(client_socket):
 
             elif process_url(url=url) == 'delete':
                 # Delete Method
-                delete(client_socket=client_socket, url=url, received_data=received_data, username=username)
+                delete(client_socket=client_socket, url=url,
+                       received_data=received_data, username=username)
 
             else:
                 # Authorization header not provided
@@ -410,8 +545,9 @@ def handle_client_request(client_socket):
         #         print("close")
 
     if detectedClose:
-      
+
         return "Bye"
+
 
 def process_path(raw_path):
     # Unquote the path to handle percent-encoded characters
@@ -452,8 +588,10 @@ def upload(client_socket, url, received_data, username, headers):
         content_disposition = headers.get("Content-Disposition")
         if content_disposition:
             # Manual parsing of Content-Disposition header
-            params = [param.strip() for param in content_disposition.split(";")]
-            filename_param = next((param for param in params if param.lower().startswith("filename")), None)
+            params = [param.strip()
+                      for param in content_disposition.split(";")]
+            filename_param = next(
+                (param for param in params if param.lower().startswith("filename")), None)
 
             if filename_param:
                 _, filename = filename_param.split("=")
